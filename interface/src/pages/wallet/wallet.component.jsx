ef4b3afaa40305015407b88dcd63e6af0f17a38f
GeoStefan <georgianspopescu@gmail.com>
2019-11-13 08:34:45 +0200

    Modify wallet page for presentation

--- interface/src/pages/wallet/wallet.component.jsx
+++ interface/src/pages/wallet/wallet.component.jsx
@@ -6,19 +6,39 @@ export default class Wallet extends React.Component {
         super(props);
         this.state = {
             accountAddress: '',
-            identities: []
+            identities: [],
+            value: '',
+            txHash: ''
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     async componentDidMount() {
         let accountAddress = await getAccountAddress();
-        // let result = await createIdentity("122345", "0x91ba5ac30060b1b33b953186B04D4F540B717D9c");
+        // await createIdentity(accountAddress, "435644", "0x91ba5ac30060b1b33b953186B04D4F540B717D9c");
         this.setState({ accountAddress: accountAddress });
         let result = await getAllIdentities(this.state.accountAddress);
         console.log(result);
         this.setState({ identities: result });
         let ip = await getAllIdentityProviders();
         console.log(ip);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    async handleSubmit(event) {
+        let tx;
+        try { tx = await createIdentity(this.state.accountAddress, this.state.value, "0x91ba5ac30060b1b33b953186B04D4F540B717D9c"); }
+        catch (err) {
+            console.log(err);
+        }
+        this.setState({ txHash: tx });
+        let result = await getAllIdentities(this.state.accountAddress);
+        this.setState({ identities: result });
+        event.preventDefault();
     }
 
     renderIdentities = () => {
@@ -37,6 +57,13 @@ export default class Wallet extends React.Component {
             <ul>
                 {ids}
             </ul>
+
+            <label>
+                IPFS hash:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <button type="submit" onClick={this.handleSubmit} >Add</button>
+            <div>{this.state.txHash}</div>
         </div>;
     }
 }
